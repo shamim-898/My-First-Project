498d02859d8289dfe1592a86bb0db6e67db85b46
Md Shamim Mia <shamim.dev98@gmail.com>
2026-09-26 00:52:53 +0600

    Fix spelling errors in input prompts and outputs
    Corrected spelling errors in prompts and outputs.

--- Input print.c
+++ Input print.c
@@ -1,1 +1,22 @@
+ #include<stdio.h>
+ int main()
+ {
+  int a;
+  float f;
+  char c;
+  double d;
+  printf("Enter the intiser vlue,\n");
+  scanf("%d",&a);
+  printf("Enter the float vlue,\n");
+  scanf(" %f",&f);
+  printf("Enter the charectur vlue,\n");
+  scanf(" %c", &c);
+  printf("Enter the double vlue,\n");
+  scanf(" %lf",&d);
+  printf("intiser vlue:%d\n",a);
+  printf("float vlue, %.2f\n",f);
+  printf(" charectur vlue,%c\n",c);
+  printf("double vlue,%.3lf\n",d);
+  return 0;
+ }
 
